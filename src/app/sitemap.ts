0c42e358ef2e3ed445c53bc5321893c3fca930b7
Lars Kristian Haga <lars.kristian.haga@cappelendamm.no>
2025-09-03 14:13:56 +0200

    Added robots and sitemap pages

---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import {MetadataRoute} from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://3fs.no"
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/services`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.6,
+        },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.7,
+        },
+    ]
+}
