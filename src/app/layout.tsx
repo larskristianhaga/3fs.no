c9b58f7daef831fee1ed0242d1a786bf0ecde953
Lars Kristian Haga <lars.kristian.haga@cappelendamm.no>
2025-09-03 13:58:30 +0200

    Added Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
+import {Analytics} from '@vercel/analytics/react'
 import './globals.css'
 
 const inter = Inter({subsets: ['latin']})
@@ -22,6 +23,7 @@ export default function RootLayout({
         <html lang="en">
         <body className={inter.className}>
         {children}
+        <Analytics/>
         </body>
         </html>
     )
