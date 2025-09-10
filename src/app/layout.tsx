95aea45c0922156cf3088911508801e66260eac4
Lars Kristian Haga <lars.kristian.haga@cappelendamm.no>
2025-09-10 09:00:49 +0200

    Changed language to norwegian

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="no">
         <body className={inter.className}>
         {children}
         <Analytics/>
